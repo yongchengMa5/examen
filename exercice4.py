5e51591872165c1faca78012250c3b2012d6cf45
mathieu <ychengma@gmail.com>
2022-02-01 16:17:21 +0100

    ajouter les fichiers

--- exercice4.py
+++ exercice4.py
@@ -1,1 +1,10 @@
 # -*- coding : utf-8 -*-
+
+################
+###Exercice 4#####
+################
+#git remote add examen git@github.com:yongchengMa5/examen.git
+#git add *
+#git commit -m 'ajouter les fichiers'
+#git push examen master
+#https://github.com/yongchengMa5/examen/tree/master
